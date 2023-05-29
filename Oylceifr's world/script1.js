2a4b99cfa58807a098bad191b5e971c9ee66c366
meng <2603064062@qq.com>
2023-05-29 18:08:23 +0800

    update

---
+++ Oylceifr's world/script1.js
@@ -0,0 +1,12 @@
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
+$(function () {
+  $('.example-popover').popover({
+    container: 'body'
+  })
+})
+
+function sayHello(){
+  alert("Thanks for your lightworks!💕");
+}
